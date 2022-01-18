228aa19510b4e2dd55989c485f6fbaf4602a6a80
harroo <harroo@protonmail.com>
2022-01-18 18:18:45 +1100

    clean up some things

--- haroscan-project/Assets/Scripts/SimpleTest.cs
+++ haroscan-project/Assets/Scripts/SimpleTest.cs
@@ -42,101 +42,12 @@ public class SimpleTest : MonoBehaviour {
 
     private void Update () {
 
-        if (timer < 0) { timer = 1.0f;
-
-            // Texture2D uncroppedTexture = new Texture2D(widthRes, heightRes);
-            // uncroppedTexture.SetPixels32(camTexture.GetPixels32());
-            // Texture2D croppedTexture = TextureTools.ResampleAndCrop(uncroppedTexture, widthRes / 3, heightRes);
-            // pixelArray = croppedTexture.GetPixels32();
-
-            // widthRes = croppedTexture.width;
-            // heightRes = croppedTexture.height;
-
-            // IBarcodeReader reader = new BarcodeReader{
-            //
-            //     AutoRotate = true,
-            //
-            //     Options = new Auki.Barcode.Common.DecodingOptions{
-            //
-            //         TryHarder = true
-            //     }
-            // };
-            // var result = reader.Decode(camTexture.GetPixels32(), widthRes, heightRes);
-            // if (result != null)
-            //     resultText.text = result.Text;
-            // else resultText.text = "failed" + (x++);
-
-            // display.texture = croppedTexture;
-
-        } else timer -= Time.deltaTime;
-
         if (cache != output) {
 
             cache = output;
 
             resultText.text = output;
         }
-
-        // if (pixelArray == null)
-        //     pixelArray = camTexture.GetPixels32();
-
-        // if (Input.GetKey(KeyCode.R)) {
-        //
-        //     Texture2D texture = new Texture2D(2, 2);
-        //     texture.LoadImage(System.IO.File.ReadAllBytes("/home/haro/Downloads/barcode1.png"));
-        //
-        //     widthRes = texture.width;
-        //     heightRes = texture.height;
-        //
-        //     display.texture = texture;
-        //
-        //     pixelArray = texture.GetPixels32();
-        //
-        //     // IBarcodeReader reader = new BarcodeReader();
-        //     //
-        //     // var result = reader.Decode(pixelArray, texture.width, texture.height);
-        //     // if (result != null) {
-        //     //
-        //     //     CheckResult(result.Text);
-        //     // }
-        //
-        //     // Texture2D cropTexture = new Texture2D(widthRes, heightRes);
-        //     // int third = cropTexture.height / 3;
-        //     // Texture2D newTexture = new Texture2D(widthRes, third);
-        //     //
-        //     // cropTexture.SetPixels32(pixelArray);Debug.Log(third + " " + newTexture.width + " " + newTexture.height +  " "+ cropTexture.width + " " + cropTexture.height +  " ");
-        //     //
-        //     // for (int y = 0; y < third; ++y){
-        //     //     for (int x = 0; x < cropTexture.width; ++x){
-        //     //         newTexture.SetPixel(x, y, cropTexture.GetPixel(x, third + y));}}
-        //     //
-        //     // newTexture.Apply();
-        //     // pixelArray = newTexture.GetPixels32();
-        //
-        //     //Debug.Log("asd");
-        //
-        //     Texture2D uncroppedTexture = new Texture2D(widthRes, heightRes);
-        //     uncroppedTexture.SetPixels32(pixelArray);
-        //     Texture2D croppedTexture = TextureTools.ResampleAndCrop(uncroppedTexture, widthRes, heightRes / 3);
-        //     pixelArray = croppedTexture.GetPixels32();
-        //     widthRes = croppedTexture.width;
-        //     heightRes = croppedTexture.height;
-        //
-        //     IBarcodeReader reader = new BarcodeReader{
-        //
-        //         AutoRotate = true,
-        //
-        //         Options = new Auki.Barcode.Common.DecodingOptions{
-        //
-        //             TryHarder = true
-        //         }
-        //     };
-        //     var result = reader.Decode(pixelArray, croppedTexture.width, croppedTexture.height);
-        //     if (result != null)
-        //         resultText.text = result.Text;
-        //
-        //     display.texture = croppedTexture;
-        // }
     }
 
     private string cache, output;
